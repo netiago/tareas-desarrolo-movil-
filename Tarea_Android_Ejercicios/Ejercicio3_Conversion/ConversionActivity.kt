cfd5d4b710cd30254701a5d99a63bdd73c6f0331
netiago <jsvanegasa@gmail.com>
2026-03-03 19:03:47 -0500

    Update Conversion Activity

--- Tarea_Android_Ejercicios/Ejercicio3_Conversion/ConversionActivity.kt
+++ Tarea_Android_Ejercicios/Ejercicio3_Conversion/ConversionActivity.kt
@@ -1,1 +1,42 @@
+package com.example.myapplication
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/*
+la abuela quiere hacer una torta pero el horno esta en fahrenheit
+conversion de grados Fahrenheit a Celsius
+ */
+
+class ConversionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_conversion)
+
+        val etFahrenheit = findViewById<EditText>(R.id.etFahrenheit)
+        val btnConvertir = findViewById<Button>(R.id.btnConvertir)
+        val tvResultadoC = findViewById<TextView>(R.id.tvResultadoC)
+
+        btnConvertir.setOnClickListener {
+
+            val textoF = etFahrenheit.text.toString()
+
+            if (textoF.isNotEmpty()) {
+
+                val F = textoF.toDouble()
+
+                val C = (5.0 / 9.0) * (F - 32.0)
+
+                tvResultadoC.text = " $C C"
+
+            } else {
+
+                tvResultadoC.text = "Ingrese un valor"
+            }
+        }
+    }
+}
